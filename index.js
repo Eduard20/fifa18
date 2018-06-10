82c851033304927b7a49f5c417ac4b0bedc56dee
Eduard Simonyan <esimonyan2014@gmail.com>
2018-06-11 01:55:01 +0400

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const rp = require('request-promise-native');
+const _ = require('lodash');
+const baseUrl = 'http://www.dssclub.com.ua/fifa18/';
+let GROUPS = {};
+
+app.get('/groups', async (req, res) => {
+    if (!_.isEmpty(GROUPS)) {
+        console.log(1);
+        return res.send(GROUPS);
+    }
+    const countriesAray = await rp(`${baseUrl}groupup.php`, {json: true});
+    const modifiedArray = countriesAray.map(country => {
+        return {
+            ...country,
+            logolink: baseUrl + country.logolink
+        }
+    });
+    const groupedObj = _.groupBy(modifiedArray, country => country.gr);
+    GROUPS = {...groupedObj};
+    return res.send(groupedObj);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`listening on port ${port}`));
